b069ee43d3b9bbff9c4b70dda5a15fe970f53e50
samgende <sam.gend1415@gmail.com>
2022-02-27 19:39:47 +0100

    refractor to work with firbase maybe

--- App.js
+++ App.js
@@ -2,23 +2,44 @@ import { StatusBar } from 'expo-status-bar';
 import {Image, StyleSheet, Text, View, TouchableOpacity, TextInput} from 'react-native';
 import PizzaTranslator from './Inputs'
 import  hspLogo from './assets/hsp.png'
+import { useState } from 'react';
 
 export default function App() {
+
+  const[email, setEmail] = useState();
+  const[password, setPassword] = useState();
+
+
   return (
     <View style={styles.container}>
       <Image source={hspLogo} style={styles.hspLogo}/>
       <Text style={styles.text}>FFW</Text>
       <StatusBar style="auto" />
   
-       <PizzaTranslator label='Username'></PizzaTranslator>
-       <PizzaTranslator label='Passwort'></PizzaTranslator>
+       <Text style={styles.text}>Email</Text>
+       <TextInput
+       style={{height:40}}
+       onChangeText={text => setEmail(text)}
+       ></TextInput>
+
+       <Text style={styles.text}>Password</Text>
+       <TextInput
+       style={{height:40}}
+       onChangeText={text => setPassword(text)}
+       ></TextInput>
 
       <TouchableOpacity 
-      onPress={() => alert('Login')}
+      onPress={() => alert(password)}
       style={styles.button}
       >
         <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
 
+      <TouchableOpacity 
+      onPress={() => alert(password)}
+      style={styles.button}
+      >
+        <Text style={styles.buttonText}>Register</Text>
       </TouchableOpacity>
     </View>
   );
@@ -40,6 +61,7 @@ const styles = StyleSheet.create({
     fontSize: 28
   },
   button: {
+    margin:10,
     backgroundColor:'#13f702',
     padding: 10
   },
